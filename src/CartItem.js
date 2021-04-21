04f3d02ce5f838e63a781e44e40788b9bd3a95d7
Joy-deb <19bcs2264@cuchd.in>
2021-04-21 08:10:52 +0530

    Added setState to increase and decrease quantity

--- src/CartItem.js
+++ src/CartItem.js
@@ -6,7 +6,7 @@ class CartItem extends React.Component {
         this.state = {
             price: 9999,
             title: 'Mobile Phone',
-            qty: '1',
+            qty: 1,
             img: ''
         }
         /* Binding - (If we have multiple event handler we can bind over here i.e Constructor.)
@@ -15,11 +15,37 @@ class CartItem extends React.Component {
         */
         }
     // Arrow function will automatically bind the instance with the value
+    decreaseQuantity = () => {
+        // As we don't want the quantity to be less 0
+        // const qty = this.state.qty; or
+        const {qty} = this.state;
+        if(qty === 0){
+            return;
+        }
+        this.setState((prevState) =>{
+            return{
+                qty: prevState.qty - 1
+            }
+        });
+    } 
     increaseQuantity = () => {
-        console.log('this.state', this.state);
+        // console.log('this.state', this.state);
+        // First Form - if we dont need prevState
+        // this.setState({
+        //     qty: this.state.qty+1
+        // })
+        // Second Form (Function)- if need to modify prevState
+        this.setState((prevState)=>{
+            return{
+                qty: prevState.qty + 1
+            }
+        }, ()=>{
+            console.log(this.state.qty); 
+        });
     }
     render() {
-        //Grabbing the values from this.state
+        // console.log('render');
+        //Grabbing the values from this.state ( De-Structure )
         const { title, price, qty} = this.state;
         return (
             <div className="cart-item">
@@ -33,20 +59,21 @@ class CartItem extends React.Component {
                     <div className="cart-item-actions">
                         {/*button*/}
                         <img 
-                            alt="decrease" 
+                            alt="increase" 
                             className="action-icons" 
-                            src="https://www.flaticon.com/svg/vstatic/svg/992/992651.svg?token=exp=1618901922~hmac=1ec3865e73678fc3001bc0cb967c7cc9"
+                            src="https://www.flaticon.com/svg/vstatic/svg/992/992651.svg?token=exp=1618965587~hmac=e3c4f4a0cfb9b5544f2309607c9f792f"
                             onClick={this.increaseQuantity}
                         />
                         <img 
-                            alt="increase" 
+                            alt="decrease" 
                             className="action-icons" 
-                            src="https://www.flaticon.com/svg/vstatic/svg/992/992683.svg?token=exp=1618901846~hmac=a679885032a37d94927d08b3e3df1b6e"
+                            src="https://www.flaticon.com/svg/vstatic/svg/992/992683.svg?token=exp=1618965402~hmac=59c17168caab957ba797590eea4322e7"
+                            onClick={this.decreaseQuantity}
                         />
                         <img 
                             alt="delete" 
                             className="action-icons" 
-                            src="https://www.flaticon.com/svg/vstatic/svg/1345/1345823.svg?token=exp=1618901946~hmac=68dddf6813e0805d45b34ceec0917e72"
+                            src="https://www.flaticon.com/svg/vstatic/svg/1345/1345823.svg?token=exp=1618965527~hmac=abd5cc9e39fe8048896b66bc8f89668a"
                         />
                     </div>
                 </div>
