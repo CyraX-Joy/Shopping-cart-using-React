35e376782dfe64681ca6e4be253c24301c2dc385
Joy-deb <19bcs2264@cuchd.in>
2021-05-20 12:58:45 +0530

    Adding Increasing , decreasing and Delete Event Handling in Cart

--- src/Cart.js
+++ src/Cart.js
@@ -9,62 +9,95 @@ class Cart extends React.Component {
       Items: [
         { 
             id: 1, 
-            price: 27999, 
+            price: 599, 
             title: "Apple Ipad 8th gen", 
             qty: 1, 
             img: "" 
         },
         { 
             id: 2, 
-            price: 15999, 
+            price: 349, 
             title: "Apple Watch 3 42mm", 
             qty: 2, 
             img: "" 
         },
         { 
             id: 3, 
-            price: 79999,
+            price: 999,
             title: "Iphone 12 Pro 128GB", 
             qty: 4, 
             img: "" 
         },
         {
           id: 4,
-          price: 41999,
+          price: 649,
           title: "Apple Over-the-Ear Headphones",
           qty: 1,
           img: "",
         },
         {
           id: 5,
-          price: 119999,
+          price: 1299,
           title: "Apple Macbook Air 512GB",
           qty: 2,
           img: "",
         },
         { 
             id: 6, 
-            price: 17999, 
+            price: 179, 
             title: "Apple Airpods 2", 
             qty: 4, 
             img: "" 
         },
         { 
             id: 7, 
-            price: 7999, 
+            price: 89, 
             title: "Apple Pencil 2", 
             qty: 1, 
             img: "" 
         },
         {
           id: 8,
-          price: 599,
+          price: 9,
           title: "Apple Itunes Membership Monthly",
           qty: 5,
           img: "",
         },
       ],
     };
+  }
+  handleIncreaseQuantity = (product) => {
+    //   console.log("Please Increase the Quantity of ", product);
+    const { Items } = this.state;
+    const index = Items.indexOf(product);
+    Items[index].qty += 1;
+    this.setState({
+        Items
+    })
+  }
+  handleDecreaseQuantity = (product) => {
+    //   console.log("Please Increase the Quantity of ", product);
+    const { Items } = this.state;
+    const index = Items.indexOf(product);
+    //We are grabbing the Items from the state and finding the index of the clicked
+    //Product and perform the action and setState accordingly.
+    console.log(Items[index].qty)
+    if(Items[index].qty > 1){
+        Items[index].qty -= 1;
+        this.setState({
+            Items
+        })
+    }else{
+        Items[index].qty = 0;
+        this.handleDeleteQuantity(Items[index].id);
+    }
+  }
+  handleDeleteQuantity = (id) => {
+      const { Items } = this.state;
+      const products = Items.filter((item)=>item.id!=id); // Return array of item after deleting that product
+      this.setState({
+          Items : products
+      })
   }
   render() {
     const { Items } = this.state;
@@ -73,7 +106,10 @@ class Cart extends React.Component {
         {Items.map((item) => {
           return (<CartItem 
                    item={item} 
-                   key={item.id} 
+                   key={item.id}
+                   onIncreaseQuantity={this.handleIncreaseQuantity}
+                   onDecreaseQuantity={this.handleDecreaseQuantity}
+                   onDeleteQuantity={this.handleDeleteQuantity}
                  />);
         })}
       </div>
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,97 +1,99 @@
-import React from 'react';
-import { AiFillPlusCircle, AiFillMinusCircle,  AiFillDelete} from "react-icons/ai";
+import React from "react";
+import {
+  AiFillPlusCircle,
+  AiFillMinusCircle,
+  AiFillDelete,
+} from "react-icons/ai";
 
 class CartItem extends React.Component {
-    constructor() {
+    //   constructor() {
         // Everytime we are adding Contructor to our classes in react, we
         // we need to add super() (Calling parent class)
-        super();
-        this.state = {
-            price: 9999,
-            title: 'Mobile Phone',
-            qty: 1,
-            img: ''
-        }
+    //     super();
+    //     this.state = {
+    //       price: 9999,
+    //       title: "Mobile Phone",
+    //       qty: 1,
+    //       img: "",
+    //     };
         /* Binding - (If we have multiple event handler we can bind over here i.e Constructor.)
-        Or can use arrow function instead of normal ones.
-        this.increaseQuantity = this.increaseQuantity.bind(this); 
-        */
-    }
-    // Arrow function will automatically bind the instance with the value
-    decreaseQuantity = () => {
+            Or can use arrow function instead of normal ones.
+            this.increaseQuantity = this.increaseQuantity.bind(this); 
+            */
+    //   }
+      // Arrow function will automatically bind the instance with the value
+    //   decreaseQuantity = () => {
         // As we don't want the quantity to be less 0
         // const qty = this.state.qty; or
-        const {qty} = this.state;
-        if(qty === 0){
-            return;
-        }
-        this.setState((prevState) =>{
-            return{
-                qty: prevState.qty - 1
-            }
-        });
-    } 
-    increaseQuantity = () => {
+    //     const { qty } = this.state;
+    //     if (qty === 0) {
+    //       return;
+    //     }
+    //     this.setState((prevState) => {
+    //       return {
+    //         qty: prevState.qty - 1,
+    //       };
+    //     });
+    //   };
+    //   increaseQuantity = () => {
         // console.log('this.state', this.state);
         // First Form - if we don't need prevState
         // this.setState({
         //     qty: this.state.qty+1
         // })
         // Second Form (Function)- if need to modify prevState
-        this.setState((prevState)=>{
-            return{
-                qty: prevState.qty + 1
-            }
-        }, ()=>{
-            console.log(this.state.qty); 
-        });
-    }
-    render() {
-        // this.setState({qty: this.state.qty +1 }); we can't use setState inside render
-        // Because it again invoke render() and again ... (Infinite loop) leads to stack overflow. 
+    //     this.setState(
+    //       (prevState) => {
+    //         return {
+    //           qty: prevState.qty + 1,
+    //         };
+    //       },
+    //       () => {
+    //         console.log(this.state.qty);
+    //       }
+    //     );
+    //   };
+  render() {
+    // this.setState({qty: this.state.qty +1 }); we can't use setState inside render
+    // Because it again invoke render() and again ... (Infinite loop) leads to stack overflow.
 
-        // console.log('render');
-        //Grabbing the values from this.state ( De-Structure )
-        const { title, price, qty} = this.props.item;
-        return (
-            <div className="cart-item">
-                <div className="left-block">
-                    <img style={styles.image }/>
-                </div>
-                <div className="right-block">
-                    <div style={ {fontSize : 25} }> {title} </div>
-                    <div style={ {color : '#777' } }> Rs:{price} </div>
-                    <div style={ {color : '#777' } }> Quantity: {qty} </div>
-                    <div className="cart-item-actions">
-                        {/*button*/}
-                        {/* <img 
-                            alt="increase" 
-                            className="action-icons" 
-                            src="https://www.flaticon.com/svg/vstatic/svg/992/992651.svg?token=exp=1620055713~hmac=19e0e8f32b3b452d5445819cc4af6e89"
-                            onClick={this.increaseQuantity}
-                        /> */}
-                        <div className="action-icons" onClick={this.increaseQuantity} >
-                            <AiFillPlusCircle />
-                        </div>
-                        <div className="action-icons" onClick={this.decreaseQuantity}>
-                            <AiFillMinusCircle />
-                        </div>
-                        <div className="action-icons" onClick={this.decreaseQuantity}>
-                            <AiFillDelete />
-                        </div>
-                    </div>
-                </div>
+    // console.log('render');
+    //Grabbing the values from this.state ( De-Structure )
+    const { title, price, qty, id} = this.props.item;
+    const { onIncreaseQuantity, onDecreaseQuantity, onDeleteQuantity, item} = this.props;
+    return (
+      <div className="cart-item">
+        <div className="left-block">
+          <img style={styles.image} />
+        </div>
+        <div className="right-block">
+          <div style={{ fontSize: 25 }}> {title} </div>
+          <div style={{ color: "#777" }}> $:{price} </div>
+          <div style={{ color: "#777" }}> Quantity: {qty} </div>
+          <div className="cart-item-actions">
+            {/*button*/}
+            <div className="action-icons" onClick={() => onIncreaseQuantity(item)}>
+              <AiFillPlusCircle />
             </div>
-        );
-    }
+            <div className="action-icons" onClick={() => onDecreaseQuantity(item)}>
+              <AiFillMinusCircle />
+            </div>
+            <div className="action-icons" onClick={() => onDeleteQuantity(id)}>
+              <AiFillDelete />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const styles = {
-    image: {
-        height: 110,
-        width: 110,
-        borderRadius: 4,
-        background: '#ccc'
-    }
-}
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    background: "#ccc",
+  },
+};
 export default CartItem;
